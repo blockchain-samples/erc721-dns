34866b988acbb9924d104731ea1248e73d45682d
root <ilyapt4@gmail.com>
2017-07-07 15:21:55 +0000

    checking domain name

--- simpleDNS.sol
+++ simpleDNS.sol
@@ -7,18 +7,26 @@ contract simpleDNS {
     }
  
     mapping (string => Record) records;
+
+    modifier isDomainName (string domain) {
+        bytes memory tmp = bytes(domain);
+        for (uint i = 0; i < tmp.length; i++) {
+            require(tmp[i] == 45 || (tmp[i] >= 48 && tmp[i] <= 57) || (tmp[i] >= 97 && tmp[i] <= 122));
+        }
+        _;
+    }
     
-    function setDomain(string _domain, string _ipaddr) {
+    function setDomain(string _domain, string _ipaddr) isDomainName(_domain) {
         require(records[_domain].owner == address(0x0) || records[_domain].owner == msg.sender);
         records[_domain] = Record(msg.sender, _ipaddr);
     }
     
-    function getDomain(string _domain) constant returns(string) {
+    function getDomain(string _domain) isDomainName(_domain) constant returns(string) {
         return records[_domain].ipaddr;
     }
     
-    function transfer(string _domain, address _to) {
+    function transfer(string _domain, address _to) isDomainName(_domain) {
         require(records[_domain].owner == msg.sender);
         records[_domain].owner = _to;
-    }    
+    }
 }
