58d781458e705e7eb44719f7810818ff2d23fbd8
root <ilyapt4@gmail.com>
2017-10-02 18:37:47 +0000

    rewrited tests with async/await

--- test/1_test.js
+++ test/1_test.js
@@ -1,108 +1,118 @@
 const BlockchainDNS = artifacts.require("./BlockchainDNS.sol");
 
-contract('Simple test', (accounts) => {
+contract('Blockchain Domain infrastructure test', (accounts) => {
     let dns;
 
-    before((done) => {
-        BlockchainDNS.deployed().then((_dns) => {
-            dns = _dns;
-            done();
-        });
+    before(async () => {
+        dns = await BlockchainDNS.deployed()
     });
 
-    it('Set Google name servers', (done) => {
-        dns.domainSet('google', [134744072, 134743044]).then(() => {
-            done();
-        });
+    it('Set Google name servers', async () => {
+        await dns.domainSet('google', [134743044, 134744072]);
     });
 
-    it('Check Google name servers count', (done) => {
-        dns.domainServersCount('google').then((count) => {
-            assert.equal(count.toNumber(), 2);
-            done();
-        });
+    it('Check Google', async () => {
+        let count = dns.domainServersCount('google');
+        let srv1 = dns.domainServer('google', 0);
+        let srv2 = dns.domainServer('google', 1);
+        assert.equal((await count).toNumber(), 2);
+        srv1 = await srv1;
+        srv2 = await srv2;
+        assert.equal(srv1[0].toNumber(), 134743044);
+        assert.equal(srv1[1].toString(), '8.8.4.4');
+        assert.equal(srv2[0].toNumber(), 134744072);
+        assert.equal(srv2[1].toString(), '8.8.8.8');
     });
 
-    it('Check Google first name server', (done) => {
-        dns.domainServer('google', 0).then((server) => {
-            assert.equal(server[0].toNumber(), 134744072);
-            assert.equal(server[1].toString(), '8.8.8.8');
-            done();
-        });
+    it('Set Comodo name servers', async () => {
+        await dns.domainSet('comodo', [135936026, 135591700]);
     });
 
-    it('Check Google second name server', (done) => {
-        dns.domainServer('google', 1).then((server) => {
-            assert.equal(server[0].toNumber(), 134743044);
-            assert.equal(server[1].toString(), '8.8.4.4');
-            done();
-        });
+    it('Check Comodo', async () => {
+        let count = dns.domainServersCount('comodo');
+        let srv1 = dns.domainServer('comodo', 0);
+        let srv2 = dns.domainServer('comodo', 1);
+        assert.equal((await count).toNumber(), 2);
+        srv1 = await srv1;
+        srv2 = await srv2;
+        assert.equal(srv1[0].toNumber(), 135936026);
+        assert.equal(srv1[1].toString(), '8.26.56.26');
+        assert.equal(srv2[0].toNumber(), 135591700);
+        assert.equal(srv2[1].toString(), '8.20.247.20');
     });
 
-    it('Set Comodo name servers', (done) => {
-        dns.domainSet('comodo', [135936026, 135591700]).then(() => {
-            done();
-        });
+    it('Update Google name servers', async () => {
+        await dns.domainSet('google', [134744072, 134743044]);
     });
 
-    it('Check Comodo first name server', (done) => {
-        dns.domainServer('comodo', 0).then((server) => {
-            assert.equal(server[0].toNumber(), 135936026);
-            assert.equal(server[1].toString(), '8.26.56.26');
-            done();
-        });
+    it('Check Google again', async () => {
+        let count = dns.domainServersCount('google');
+        let srv1 = dns.domainServer('google', 0);
+        let srv2 = dns.domainServer('google', 1);
+        assert.equal((await count).toNumber(), 2);
+        srv1 = await srv1;
+        srv2 = await srv2;
+        assert.equal(srv1[0].toNumber(), 134744072);
+        assert.equal(srv1[1].toString(), '8.8.8.8');
+        assert.equal(srv2[0].toNumber(), 134743044);
+        assert.equal(srv2[1].toString(), '8.8.4.4');
     });
 
-    it('Check first account domains count', (done) => {
-        dns.domainListingLen(accounts[0]).then((count) => {
-            assert.equal(count.toNumber(), 2);
-            done();
-        });
+    it('Check first account domains', async () => {
+        let count = dns.domainListingLen(accounts[0]);
+        let domain1 = dns.domainListing(accounts[0], 0);
+        let domain2 = dns.domainListing(accounts[0], 1);
+        assert.equal((await count).toNumber(), 2);
+        assert.equal((await domain1).toString(), 'google');
+        assert.equal((await domain2).toString(), 'comodo');
     });
 
-    it('Check first account first domain', (done) => {
-        dns.domainListing(accounts[0], 0).then((domain) => {
-            assert.equal(domain, 'google');
-            done();
-        });
+    it('Transfer Google to second account', async () => {
+        await dns.transfer('google', accounts[1]);
     });
 
-    it('Check first account second domain', (done) => {
-        dns.domainListing(accounts[0], 1).then((domain) => {
-            assert.equal(domain, 'comodo');
-            done();
-        });
+    it('Check first account domains', async () => {
+        let count = dns.domainListingLen(accounts[0]);
+        let domain = dns.domainListing(accounts[0], 0);
+        assert.equal((await count).toNumber(), 1);
+        assert.equal((await domain).toString(), 'comodo');
     });
 
-    it('Transfer google domain to second account', (done) => {
-        dns.transfer('google', accounts[1]).then(() => done());
+    it('Check second account domains', async () => {
+        let count = dns.domainListingLen(accounts[1]);
+        let domain = dns.domainListing(accounts[1], 0);
+        assert.equal((await count).toNumber(), 1);
+        assert.equal((await domain).toString(), 'google');
     });
 
-    it('Check first account domains count', (done) => {
-        dns.domainListingLen(accounts[0]).then((count) => {
-            assert.equal(count.toNumber(), 1);
-            done();
-        });
+    it('Second account ordering google to sell', async () => {
+        await dns.addSellOrder('google', 5000, {from: accounts[1]});
     });
 
-    it('Check first account domain', (done) => {
-        dns.domainListing(accounts[0], 0).then((domain) => {
-            assert.equal(domain, 'comodo');
-            done();
-        });
+    it('Check google in sell orders', async () => {
+        let count = (await dns.sellOrdersLen()).toNumber();
+        let order = await dns.orders(0);
+        assert.equal(count, 1);
+        assert.equal(order[0].toString(), 'google');
+        assert.equal(order[1].toNumber(), 5000);
     });
 
-    it('Check second account domains count', (done) => {
-        dns.domainListingLen(accounts[1]).then((count) => {
-            assert.equal(count.toNumber(), 1);
-            done();
-        });
+    it('First account execute order and buy google', async () => {
+        await dns.orderBuy('google', {value: 5000});
     });
 
-    it('Check second account domain', (done) => {
-        dns.domainListing(accounts[1], 0).then((domain) => {
-            assert.equal(domain, 'google');
-            done();
-        });
+    it('Check first account domains', async () => {
+        let count = dns.domainListingLen(accounts[0]);
+        let domain1 = dns.domainListing(accounts[0], 0);
+        let domain2 = dns.domainListing(accounts[0], 1);
+        assert.equal((await count).toNumber(), 2);
+        assert.equal((await domain1).toString(), 'comodo');
+        assert.equal((await domain2).toString(), 'google');
+    });
+
+    it('Check no sell orders', async () => {
+        let count = (await dns.sellOrdersLen()).toNumber();
+        assert.equal(count, 0);
     });
 });
+
